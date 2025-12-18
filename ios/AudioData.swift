249aac9b87d2a5bb10b8eb817ac58a28fa13f2d1
Mopriestt <mopriestt@gmail.com>
2025-12-19 01:20:17 +1100

    Setup C++ Nitro Module Demo

--- ios/AudioData.swift
+++
@@ -1,5 +0,0 @@
-class AudioData: HybridAudioDataSpec {
-    public func multiply(a: Double, b: Double) throws -> Double {
-        return a * b
-    }
-}
--- src/AudioData.nitro.ts
+++ src/AudioData.nitro.ts
@@ -1,6 +1,6 @@
 import type { HybridObject } from 'react-native-nitro-modules';
 
 export interface AudioData
-  extends HybridObject<{ ios: 'swift'; android: 'kotlin' }> {
+  extends HybridObject<{ ios: 'c++'; android: 'c++' }> {
   multiply(a: number, b: number): number;
 }
